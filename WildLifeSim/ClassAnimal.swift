ab4c43ab3f34a54ac13656782aae20787681cc5a
Nik <nik-nfsu@ya.ru>
2020-05-16 16:06:03 +0300

    Added Animal class

---
+++ WildLifeSim/ClassAnimal.swift
@@ -0,0 +1,39 @@
+//
+//  ClassAnimal.swift
+//  WildLifeSim
+//
+//  Created by Николай Соломатин on 16.05.2020.
+//  Copyright © 2020 Николай Соломатин. All rights reserved.
+//
+
+import Foundation
+
+enum SizeType {
+    case small
+    case medium
+    case big
+}
+
+struct Coord {
+    var col: Int
+    var row: Int
+}
+
+class Animal {
+    var id: Int = 1
+    var name: String
+    var size: Int = 50
+    var sizeType: SizeType = .medium
+    var age: Int = 0
+    var coord: Coord
+    
+    init(myName: String, myCoord: Coord) {
+        coord = myCoord
+        name = myName
+    }
+    
+    func placeOnGround(earth: Ground) {
+        earth.tiles[coord.col][coord.row].isEmpty = false
+        print("Animal placed at \(coord)")
+    }
+}
--- WildLifeSim/GameViewController.swift
+++ WildLifeSim/GameViewController.swift
@@ -37,6 +37,11 @@ class GameViewController: UIViewController {
         earth.initTiles()
         // Env init
         env.foodCount = earth.initFoodCount
+        // Animals init
+        let coord = Coord(col: 0, row: 0)
+        var wolf = Animal(myName: "Волк 1", myCoord: coord)
+        wolf.placeOnGround(earth: earth)
+        // Enable scene
         skView.presentScene(scene)
     }
 
